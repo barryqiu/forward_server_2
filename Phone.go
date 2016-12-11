88193042e5b63454fe26f9947f42a781b255b68d
barry <qiuxiaobing90@126.com>
2016-12-11 22:56:19 +0800

    first push

--- Phone.go
+++ Phone.go
@@ -5,7 +5,6 @@ import (
 	"net"
 	"strings"
 	"sync"
-	"time"
 	"os"
 	"bufio"
 	"errors"
@@ -15,17 +14,23 @@ import (
 )
 
 type Phone struct {
-	mu             sync.Mutex
-	Conn_list      []net.TCPConn
-	User_name      string
-	Random         string
-	Last_known     string
-	Overhead       int
-	Overheat_count int
-	data_client    chan []byte
-	data_device    chan []byte
-	stop           chan int
-	client_conn    chan ClientConn
+	mu          sync.Mutex
+	Conn        net.TCPConn
+	Device_name string
+	Random      string
+	Last_known  string
+	Data_client chan []byte
+	Data_device chan []byte
+	Stop        chan int
+	Client_conn chan ClientConn
+}
+
+func RevicePhoneData(phone Phone) {
+
+}
+
+func ReadDataFromDevice(phone Phone)  {
+
 }
 
 func (phone *Phone) append_conn(conn net.TCPConn) error {
@@ -35,100 +40,27 @@ func (phone *Phone) append_conn(conn net.TCPConn) error {
 	if err != nil {
 		phone.log_to_file("set keep alive error:", err)
 	}
+	phone.Conn = conn
 
-	err = conn.SetKeepAlivePeriod(time.Second * 960)
-	if err != nil {
-		phone.log_to_file("set keep alive period error:", err)
-	}
+	// 通知云端连接建立成功
+	phone.Conn.Write([]byte("STP1\r\n0\r\n\r\n"))
 
-	phone.Conn_list = append(phone.Conn_list, conn)
+	/**
+	处理收到的各种 chan 信息
+	 */
+	go RevicePhoneData(phone)
 
-	if len(phone.Conn_list) > 4 {
-		conn0 := phone.Conn_list[0]
-		err = conn0.Close()
-		if err != nil {
-			phone.log_to_file("close conn error:", err)
-		}
-		phone.Conn_list = phone.Conn_list[1:]
-	}
+	/**
+	从 device 端接受心跳信息
+	 */
+	go ReadDataFromDevice(phone)
 
-	address := conn.RemoteAddr()
-	ip := strings.Split(address.String(), ":")[0]
-
-	if phone.Last_known != ip {
-		phone.log_to_file("not last known ip")
-		phone.log_to_file("Last known IP:" + phone.Last_known + ", new:" + ip)
-		for len(phone.Conn_list) > 1 {
-			conn0 := phone.Conn_list[0]
-			err = conn0.Close()
-			if err != nil {
-				phone.log_to_file("close conn error:", err)
-			}
-			phone.Conn_list = phone.Conn_list[1:]
-		}
-		phone.Last_known = ip
-		phone.Overheat_count = 0
-		phone.Overhead = 0
-	}
-
-	phone.log_to_file("append a conn", conn.RemoteAddr().String())
-	phone.log_to_file("phone conn list size______", len(phone.Conn_list))
-
+	phone.log_to_file("new conn", conn.RemoteAddr().String())
 	phone.mu.Unlock()
 	return nil
 }
 
-func (phone *Phone) get_conn() (conn net.TCPConn, err error) {
-	exp_time := time.Now().Add(time.Second * 1)
-	phone.mu.Lock()
-	for len(phone.Conn_list) == 0 {
-		time.Sleep(time.Millisecond * 500)
-		if exp_time.Before(time.Now()) {
-			break
-		}
-	}
 
-	if len(phone.Conn_list) == 0 {
-		phone.log_to_file("Got signal , bu no connection " + phone.User_name)
-		phone.mu.Unlock()
-		return net.TCPConn{}, errors.New("no connect")
-	}
-
-	conn0 := phone.Conn_list[0]
-	phone.Conn_list = phone.Conn_list[1:]
-
-	phone.log_to_file(phone.User_name, "return conn", conn0.RemoteAddr().String())
-
-	phone.mu.Unlock()
-	return conn0, nil
-}
-
-func (phone *Phone) close_all_conn() error {
-	phone.mu.Lock()
-	for len(phone.Conn_list) > 0 {
-		conn0 := phone.Conn_list[0]
-		err := conn0.Close()
-		if err != nil {
-			phone.log_to_file("close conn error:", err)
-		}
-		phone.Conn_list = phone.Conn_list[1:]
-	}
-	phone.mu.Unlock()
-	return nil
-}
-
-func (phone *Phone) init(user_name string, random string, conn net.TCPConn, address net.TCPAddr) error {
-	phone.User_name = user_name
-	phone.Random = random
-	phone.Last_known = ""
-	phone.Overhead = 0
-	phone.Overheat_count = 0
-	if conn == (net.TCPConn{}) {
-		phone.append_conn(conn)
-	}
-
-	return nil
-}
 
 func (phone *Phone) add_to_file() error {
 	fl, err := os.OpenFile(db_file_name, os.O_CREATE | os.O_APPEND | os.O_RDWR, 0660)
@@ -137,7 +69,7 @@ func (phone *Phone) add_to_file() error {
 		phone.log_to_file("open file error", err)
 		return err
 	}
-	fl.WriteString(phone.User_name + " " + phone.Random + "\n")
+	fl.WriteString(phone.Device_name + " " + phone.Random + "\n")
 	return nil
 }
 
@@ -145,7 +77,7 @@ func (phone *Phone) log_to_file(v ...interface{}) error {
 	string_date := current_date_string()
 	string_time := current_time_string()
 	os.MkdirAll("log" + string(filepath.Separator) + string_date, 06660)
-	log_file_name := "log" + string(filepath.Separator) + string_date + string(filepath.Separator) + phone.User_name + ".log"
+	log_file_name := "log" + string(filepath.Separator) + string_date + string(filepath.Separator) + phone.Device_name + ".log"
 	fl, err := os.OpenFile(log_file_name, os.O_CREATE | os.O_APPEND | os.O_RDWR, 0660)
 	defer fl.Close()
 	if (err != nil) {
@@ -240,7 +172,7 @@ func read_phones_from_file() {
 		infos := strings.Split(line, " ")
 		if len(infos) == 2 {
 			phone := Phone{}
-			phone.User_name = infos[0]
+			phone.Device_name = infos[0]
 			phone.Random = infos[1]
 			phones[infos[0]] = &phone
 		}
@@ -305,68 +237,67 @@ func process_phone_conn(conn net.TCPConn) {
 			return
 		}
 		infos := strings.Split(req.RequestURI, "/")
-		user_name := infos[2]
+		device_name := infos[2]
 		random := infos[3]
 		version := infos[4]
-		log.Println("reg:user_name:" + user_name + ";random:" + random + ";version:" + version)
+		log.Println("reg:device_name:" + device_name + ";random:" + random + ";version:" + version)
 
-		if len(user_name) == 0 {
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nEmpty username is not allowed."))
+		if len(device_name) == 0 {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nEmpty device name is not allowed."))
 			conn.Close()
 			return
 		}
 
-		if _, ok := phones[user_name]; ok && phones[user_name].Random != random {
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nUsername is already exists."))
+		if _, ok := phones[device_name]; ok && phones[device_name].Random != random {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nDevice name is already exists."))
 			conn.Close()
 			return
 		}
 
-		phone := Phone{}
-		phone.User_name = user_name
+		phone := Phone{Device_name:device_name, Random:random, Client_conn:make(chan ClientConn),
+			Data_client:make(chan []byte, 4096), Data_device:make(chan []byte, 4096),
+			Stop:make(chan int)}
+		phone.Device_name = device_name
 		phone.Random = random
 		phone.add_to_file()
-		phones[user_name] = &phone
+		phones[device_name] = &phone
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nOK"))
 		conn.Close()
 		return
 	}
 
-	if strings.HasPrefix(content, "WEBKEY") {
-		// WEBKEY username/random/version/port
-		// WEBKEY username/random/1.0/888
+	if strings.HasPrefix(content, "STP") {
+		// STP device_name/random
 		lines := strings.Split(content, "/r/n")
 		if len(lines) > 0 {
 			first_line := lines[0]
 			p1 := strings.Index(first_line, "/")
-			user_name := first_line[7:p1]
-			p2 := strings.Index(first_line[p1 + 1:], "/") + p1 + 1
-			random := first_line[p1 + 1 : p2]
+			device_name := first_line[4:p1]
+			random := first_line[p1 + 1 : ]
 
-			if len(user_name) <= 0 || len(random) <= 0 {
-				log.Println("user_name or random len = 0")
+			if len(device_name) <= 0 || len(random) <= 0 {
+				log.Println("device_name or random len = 0")
 				conn.Write([]byte("stop"))
 				conn.Close()
 				return
 			}
 
-			_, ok := phones[user_name];
+			_, ok := phones[device_name];
 
-			if ok && (phones[user_name].Random == random) {
+			if ok && (phones[device_name].Random == random) {
 				//log.Println(user_name, " phone append a conn", conn.RemoteAddr().String())
-				phones[user_name].append_conn(conn)
+				phones[device_name].append_conn(conn)
 				return
 			} else if !ok {
-				phone := Phone{}
-				phone.User_name = user_name
-				phone.Random = random
+				phone := Phone{Device_name:device_name, Random:random, Client_conn:make(chan ClientConn),
+					Data_client:make(chan []byte, 4096), Data_device:make(chan []byte, 4096),
+					Stop:make(chan int)}
 				phone.add_to_file()
-				phones[user_name] = &phone
-				phones[user_name].append_conn(conn)
-				log.Println("new phone ", user_name)
+				phones[device_name] = &phone
+				phones[device_name].append_conn(conn)
+				log.Println("new phone ", device_name)
 			} else {
-				log.Println(user_name, random, "stop phone old phone random: ", phones[user_name].Random)
-				//conn.Write([]byte("stop"))
+				log.Println(device_name, random, "stop phone old phone random: ", phones[device_name].Random)
 				conn.Close()
 				log.Println("no thing matched")
 				return
